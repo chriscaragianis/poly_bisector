df132260744af8e4d663a64836f4bff712fa1253
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-22 16:19:29 -0400

    switch to rightmost'

--- lib/poly_bisector.ex
+++ lib/poly_bisector.ex
@@ -5,11 +5,29 @@ defmodule PolyBisector do
 
   @doc """
   """
-  def leftmost(list) do
+  def rightmost(list) do
     List.foldr(list, hd(list), fn(x, acc) ->
-      if (hd(x) < hd(acc)) do x end
-      acc
+      cond do
+        hd(x) > hd(acc) -> x
+        true -> acc
+      end
     end)
+  end
+
+  defp cross(a, b) do
+    (Enum.at(a, 0) * Enum.at(b, 1)) - (Enum.at(a, 1)  * Enum.at(b, 0))
+  end
+
+  defp dot(a, b) do
+    (Enum.at(a, 0) * Enum.at(b, 1)) + (Enum.at(a, 1)  * Enum.at(b, 0))
+  end
+
+  defp base(a, b) do
+    [Enum.at(b, 0) - Enum.at(a, 0)]
+  end
+
+  def vector_angle(a, b) do
+    :math.atan(cross(a, b) / dot(a, b))
   end
 
   def polar_angle(p1, p2, p3) do
@@ -24,7 +42,7 @@ defmodule PolyBisector do
 
   def convex(poly) do
     point_list = MapSet.to_list(poly)
-    p_0 = leftmost(point_list)
+    p_0 = rightmost(point_list)
     hull = [p_0]
 
 
--- test/poly_bisector_test.exs
+++ test/poly_bisector_test.exs
@@ -29,14 +29,18 @@ defmodule PolyBisector.Test do
       assert PolyBisector.convex(@degenerate) == @degenerate
     end
 
-    test "leftmost" do
-      assert PolyBisector.leftmost(MapSet.to_list(@non_convex)) == [-1, 0]
+    test "rightmost" do
+      assert PolyBisector.rightmost(MapSet.to_list(@non_convex)) == [1, 0]
     end
 
     test "polar_angle" do
-      #assert_in_delta PolyBisector.polar_angle([-1, -1], [0, 0], [1, 0]), 3.92699, 0.0001;
-      #assert_in_delta PolyBisector.polar_angle([1, 1], [0, 0], [-1, 0]), 3.92699, 0.0001;
-      assert_in_delta PolyBisector.polar_angle([0, 0], [1, 2], [3, 1]), 4.71239, 0.0001;
+      assert_in_delta PolyBisector.polar_angle([-1, -1], [0, 0], [1, 0]), 3.92699, 0.0001
+      assert_in_delta PolyBisector.polar_angle([1, 1], [0, 0], [-1, 0]), 3.92699, 0.0001
+      assert_in_delta PolyBisector.polar_angle([0, 0], [1, 2], [3, 1]), 4.71239, 0.0001
+    end
+
+    test "vector_angle" do
+      assert_in_delta PolyBisector.vector_angle([-1, 3], [1, 1]), 1.1070623, 0.0001
     end
   end
 end
