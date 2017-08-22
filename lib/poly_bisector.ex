55280aa8e73976e6c2c55d399765c73367662394
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-22 14:00:17 -0400

    initial commit

---
+++ lib/poly_bisector.ex
@@ -0,0 +1,18 @@
+defmodule PolyBisector do
+  @moduledoc """
+  Documentation for PolyBisector.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PolyBisector.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poly_bisector_test.exs
@@ -0,0 +1,8 @@
+defmodule PolyBisectorTest do
+  use ExUnit.Case
+  doctest PolyBisector
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
