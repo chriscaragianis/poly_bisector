d85e6d22b3a52217f0fb2f7e712bc7fec857a07a
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-23 11:59:13 -0400

    changing approach

--- lib/poly_bisector.ex
+++ lib/poly_bisector.ex
@@ -23,21 +23,25 @@ defmodule PolyBisector do
   end
 
   defp base(a, b) do
-    [Enum.at(b, 0) - Enum.at(a, 0)]
+    [Enum.at(b, 0) - Enum.at(a, 0), Enum.at(b, 1) - Enum.at(a, 1)]
   end
 
   def vector_angle(a, b) do
-    :math.atan(cross(a, b) / dot(a, b))
+    cond do
+      a == [0, 0] -> 0
+      b == [0, 0] -> 0
+      true -> :math.atan(cross(a, b) / dot(a, b))
+    end
   end
 
   def polar_angle(p1, p2, p3) do
-    tan1 = (Enum.at(p2, 1) - Enum.at(p1, 1)) / (Enum.at(p2, 0) - Enum.at(p1, 0))
-    tan2 = (Enum.at(p3, 1) - Enum.at(p2, 1)) / (Enum.at(p3, 0) - Enum.at(p2, 0))
-    raw = :math.atan(tan2) - :math.atan(tan1)
-    cond do
-      raw < 0 -> raw + (2 * :math.pi)
-      true -> raw
-    end
+    p2_1 = base(p2, p1)
+    p3_1 = base(p3, p1)
+    p2_3 = base(p2, p3)
+    p1_3 = base(p1, p3)
+    ang1 = vector_angle(p2_1, p3_1)
+    ang2 = vector_angle(p1_3, p2_3)
+    :math.pi + ang1 + ang2
   end
 
   def convex(poly) do
--- test/poly_bisector_test.exs
+++ test/poly_bisector_test.exs
@@ -2,45 +2,113 @@ defmodule PolyBisector.Test do
   import PolyBisector
   use ExUnit.Case
 
-  @non_convex MapSet.new([
-        [0, 1],
-        [0.5, 0.5],
-        [1, 0],
-        [0.5, -0.5],
-        [0, -1],
-        [-0.5, -0.5],
-        [-1, 0],
-        [-0.5, 0.5]
-      ])
+  @non_convex [
+    [0, 1],
+    [0.5, 0.5],
+    [1, 0],
+    [0.5, -0.5],
+    [0, -1],
+    [-0.5, -0.5],
+    [-1, 0],
+    [-0.5, 0.5]
+  ]
 
-  @convex MapSet.new([
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        [-1, 0]
-      ])
+  @non_convex_split [
+    [
+      [0, 1],
+      [0.5, 0.5],
+      [1, 0],
+      [0.5, -0.5],
+      [0, -1]
+    ],
+    [
+      [0, 1],
+      [-0.5, 0.5],
+      [-1, 0],
+      [-0.5, -0.5],
+      [0, -1]
+    ]
+  ]
+
+  @convex [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+  ]
+
+  @convex_segs [
+    [
+      [0, 1],
+      [1, 0],
+    ],
+    [
+      [1, 0],
+      [0, -1],
+    ],
+    [
+      [0, -1],
+      [-1, 0],
+    ],
+    [
+      [-1, 0],
+      [0, 1],
+    ]
+  ]
+
+  @convex_split [
+    [
+      [0, 1],
+      [0, -1],
+      [-1, 0],
+    ],
+    [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+    ]
+  ]
 
   @degenerate MapSet.new([ [0, 1], [1, 1]])
 
+  defp polylist_to_set(list) do
+    setlist = Enum.map(list, fn(x) -> MapSet.new(x) end)
+    MapSet.new(setlist)
+  end
+
   describe "PolyBisector" do
+    @tag :skip
     test "convex hull" do
       assert PolyBisector.convex(@non_convex) == @convex
       assert PolyBisector.convex(@convex) == @convex
       assert PolyBisector.convex(@degenerate) == @degenerate
     end
 
+    @tag :skip
     test "rightmost" do
       assert PolyBisector.rightmost(MapSet.to_list(@non_convex)) == [1, 0]
     end
 
+    @tag :skip
     test "polar_angle" do
       assert_in_delta PolyBisector.polar_angle([-1, -1], [0, 0], [1, 0]), 3.92699, 0.0001
       assert_in_delta PolyBisector.polar_angle([1, 1], [0, 0], [-1, 0]), 3.92699, 0.0001
       assert_in_delta PolyBisector.polar_angle([0, 0], [1, 2], [3, 1]), 4.71239, 0.0001
     end
 
+    @tag :skip
     test "vector_angle" do
-      assert_in_delta PolyBisector.vector_angle([-1, 3], [1, 1]), 1.1070623, 0.0001
+      assert_in_delta PolyBisector.vector_angle([-1, 3], [1, 1]), -1.1070623, 0.0001
+    end
+
+    test "get segments" do
+      assert polylist_to_set(PolyBisector.get_segments(@convex)) == polylist_to_set(@convex_segs)
+    end
+
+    @tag :skip
+    test "split" do
+      assert polylist_to_set(PolyBisector.split(@convex)) == polylist_to_set(@convex_split)
+      assert polylist_to_set(PolyBisector.split(@non_convex)) == polylist_to_set(@non_convex_split)
     end
   end
 end
