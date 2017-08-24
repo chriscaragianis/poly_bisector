d199b3106e6f276c3f286e75b1e4e7c77d5b417b
Chris Caragianis <chriscaragianis@gmail.com>
2017-08-24 10:21:31 -0400

    remove extraneous stuff

--- lib/poly_bisector.ex
+++ lib/poly_bisector.ex
@@ -5,65 +5,12 @@ defmodule PolyBisector do
 
   @doc """
   """
-  def rightmost(list) do
-    List.foldr(list, hd(list), fn(x, acc) ->
-      cond do
-        hd(x) > hd(acc) -> x
-        true -> acc
-      end
-    end)
-  end
-
-  defp cross(a, b) do
-    (Enum.at(a, 0) * Enum.at(b, 1)) - (Enum.at(a, 1)  * Enum.at(b, 0))
-  end
-
-  defp dot(a, b) do
-    (Enum.at(a, 0) * Enum.at(b, 1)) + (Enum.at(a, 1)  * Enum.at(b, 0))
-  end
-
-  defp base(a, b) do
-    [x1, y1] = a
-    [x2, y2] = b
-    [[x2 - x1], [y2 - y2]]
-  end
-
   defp slope(point1, point2) do
     case Enum.at(point2, 0) - Enum.at(point1, 0) do
       0 -> "vert"
       _ -> (Enum.at(point2, 1) - Enum.at(point1, 1))
         / (Enum.at(point2, 0) - Enum.at(point1, 0))
     end
-  end
-
-  def vector_angle(a, b) do
-    cond do
-      a == [0, 0] -> 0
-      b == [0, 0] -> 0
-      true -> :math.atan(cross(a, b) / dot(a, b))
-    end
-  end
-
-  def polar_angle(p1, p2, p3) do
-    p2_1 = base(p2, p1)
-    p3_1 = base(p3, p1)
-    p2_3 = base(p2, p3)
-    p1_3 = base(p1, p3)
-    ang1 = vector_angle(p2_1, p3_1)
-    ang2 = vector_angle(p1_3, p2_3)
-    :math.pi + ang1 + ang2
-  end
-
-  def polar_angle_seg(seg1, seg2) do
-    [p11 = [x11, y11], p12 = [x12, y12]] = seg1
-    [p21 = [x21, y21], p22 = [x22, y22]] = seg2
-    polar_angle(p11, p21, p22)
-  end
-
-  def convex(poly) do
-    point_list = MapSet.to_list(poly)
-    p_0 = rightmost(point_list)
-    hull = [p_0]
   end
 
   def get_segments(poly) do
--- test/poly_bisector_test.exs
+++ test/poly_bisector_test.exs
@@ -77,38 +77,6 @@ defmodule PolyBisector.Test do
   end
 
   describe "PolyBisector" do
-    @tag :skip
-    test "convex hull" do
-      assert PolyBisector.convex(@non_convex) == @convex
-      assert PolyBisector.convex(@convex) == @convex
-      assert PolyBisector.convex(@degenerate) == @degenerate
-    end
-
-    @tag :skip
-    test "rightmost" do
-      assert PolyBisector.rightmost(MapSet.to_list(@non_convex)) == [1, 0]
-    end
-
-    @tag :skip
-    test "polar_angle" do
-      #assert_in_delta PolyBisector.polar_angle([-1, -1], [0, 0], [1, 0]), 3.92699, 0.0001
-      assert_in_delta PolyBisector.polar_angle([1, 1], [0, 0], [-1, 0]), 3.92699, 0.0001
-      assert_in_delta PolyBisector.polar_angle([0, 0], [1, 2], [3, 1]), 4.71239, 0.0001
-      assert_in_delta PolyBisector.polar_angle([-1, 0], [0, 1], [1, 0]), 4.71239, 0.0001
-    end
-
-    @tag :skip
-    test "polar_angle_seg" do
-      s1 = [[-1, 0], [0, 1]]
-      s2 = [[0, 1], [1, 0]]
-      assert_in_delta PolyBisector.polar_angle_seg(s1, s2), 4.71239, 0.0001
-    end
-
-    @tag :skip
-    test "vector_angle" do
-      assert_in_delta PolyBisector.vector_angle([-1, 3], [1, 1]), -1.1070623, 0.0001
-    end
-
     test "get segments" do
       assert polylist_to_set(PolyBisector.get_segments(@convex)) == polylist_to_set(@convex_segs)
     end
