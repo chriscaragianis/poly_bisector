5b3b985ef4f842d8a03f681f2b80140d3fdc9d4d
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-06 10:56:51 -0400

    changing from json to elixir map output

--- lib/GeoJSONProducer.ex
+++ lib/GeoJSONProducer.ex
@@ -1,27 +1,14 @@
 defmodule PolyBisector.GeoJSONProducer do
 
-  defp geoJSONFormatter(featuresArray) do
-    %{
-      type: "FeatureCollection",
-      features: featuresArray
-    }
-  end
-
   defp featureFormatter(coords) do
     %{
-      type: "Feature",
-      properties: %{},
-      geometry: %{
-        type: "Polygon",
-        coordinates: coords
-      }
+      type: "MultiPolygon",
+      coordinates: coords
     }
   end
 
   def toGeoJSON(input_list) do
     [featureFormatter(input_list)]
-    |> geoJSONFormatter
-    |> Poison.encode
   end
 
 end
--- test/GeoJSONProducer_test.exs
+++ test/GeoJSONProducer_test.exs
@@ -1,5 +1,6 @@
 defmodule PolyBisector.GeoJSONProducer.Test do
   use ExUnit.Case
+  import PolyBisector
   alias PolyBisector.Helpers
   alias PolyBisector.GeoJSONProducer
 
@@ -14,14 +15,36 @@ defmodule PolyBisector.GeoJSONProducer.Test do
     [-0.25, 0.25]
   ]
 
+  @bigpoly [
+    [
+      -112.81379699707031,
+      48.233477824968006
+    ],
+    [
+      -112.83233642578124,
+      48.219182942479165
+    ],
+    [
+      -112.80967712402344,
+      48.20259587674385
+    ],
+    [
+      -112.79130935668945,
+      48.22021230740323
+    ]
+  ]
+
+  describe "quick test" do
+    coords = PolyBisector.split_polys([@bigpoly], 0.0003)
+    #GeoJSONProducer.toGeoJSON(coords)
+  end
+
   describe "GeoJSONProducer" do
     test "produces valid GeoJSON" do
-      {:ok, result} = GeoJSONProducer.toGeoJSON(@non_convex)
-      {:ok, obj} = Poison.decode(result)
-      assert is_binary(result)
-      assert is_map(obj)
-      assert Map.has_key?(obj, "type")
-      assert Map.has_key?(obj, "features")
+      result = GeoJSONProducer.toGeoJSON(@non_convex)
+      assert is_list(result)
+      assert Map.has_key?(hd(result), :type)
+      assert Map.has_key?(hd(result), :coordinates)
     end
   end
 end
