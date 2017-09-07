7e93f6f15cc9881edecf279cb4c5dff0b54c3722
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-06 22:48:02 -0400

    split polys test refactored

--- test/poly_bisector_test.exs
+++ test/poly_bisector_test.exs
@@ -4,20 +4,21 @@ defmodule PolyBisector.Test do
   alias PolyBisector.Fixtures
 
   @split_polys_test_cases [
-    {[Fixtures.realsimple, Fixtures.realcomplex], 150, 3},
-    {[Fixtures.realsimple], 60, 4},
-    {[Fixtures.realcomplex], 60, 2},
-    {[Fixtures.realcomplex], 30, 3},
-    {[Fixtures.realcomplex], 10, 17},
-    {[Fixtures.realsimple, Fixtures.realcomplex], 60, 6},
-    {[Fixtures.realsimple, Fixtures.realcomplex, Fixtures.degenerate], 60, 6},
+    {0, [Fixtures.realsimple, Fixtures.realcomplex], 150, 3},
+    {1, [Fixtures.realsimple], 60, 4},
+    {2, [Fixtures.realcomplex], 60, 2},
+    {3, [Fixtures.realcomplex], 30, 3},
+    {4, [Fixtures.realcomplex], 10, 17},
+    {5, [Fixtures.realsimple, Fixtures.realcomplex], 60, 6},
+    {6, [Fixtures.realsimple, Fixtures.realcomplex, Fixtures.degenerate], 60, 6},
   ]
 
   describe "PolyBisector" do
     test "split_polys" do
       Enum.map(@split_polys_test_cases, fn(x) ->
-        {a, b, c} = x
-        assert PolyBisector.split_polys(a, b) == c
+        {n, a, b, c} = x
+        assert PolyBisector.split_polys(a, b) |> length == c,
+          ~s(split_polys case #{n} failed )
       end)
     end
   end
